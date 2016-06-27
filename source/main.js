d52338e92bd6f97ce22bd9f861a741042b7f1f06
Matt Hayes <matt@mysterycommand.com>
2016-06-26 19:55:42 -0700

    refactor: atan2 the parts against the center, update some function names

--- source/main.js
+++ source/main.js
@@ -7,7 +7,7 @@ const ctx = canvas.getContext('2d');
 const π = Math.PI;
 const ππ = 2 * Math.PI;
 
-let upDown, leftRight, around;
+let ud, lr;
 let w, h, hw, hh;
 let p = 3000;
 
@@ -22,21 +22,20 @@ function onResize(/*event*/) {
     hh = h / 2;
     hw = w / 2;
 
-    upDown = sinusoid(0 + hh / 2, h - hh / 2, p);
-    leftRight = cosinusoid(0 + hw / 2, w - hw / 2, p);
-    around = sinusoid(π / 4, π - π / 4, p / 4);
+    ud = sinWave(0 + hh / 2, h - hh / 2, p);
+    lr = cosWave(0 + hw / 2, w - hw / 2, p);
 }
 
 window.addEventListener('resize', onResize);
 onResize();
 
-function sinusoid(min, max, period, offset = 0) {
+function sinWave(min, max, period, offset = 0) {
     return function(ts) {
         return ((max - min) / 2) * (1 + Math.sin((ts + offset) * (ππ / period))) + min;
     }
 }
 
-function cosinusoid(min, max, period, offset = 0) {
+function cosWave(min, max, period, offset = 0) {
     return function(ts) {
         return ((max - min) / 2) * (1 + Math.cos((ts + offset) * (ππ / period))) + min;
     }
@@ -49,8 +48,11 @@ function draw(ctx, ts) {
     ctx.fillStyle = 'rgba(255,255,0,0.65)';
     ctx.strokeStyle = 'rgba(255,0,255,0.85)';
 
-    ctx.translate(leftRight(ts), upDown(ts));
-    ctx.rotate(around(ts));
+    const x = lr(ts);
+    const y = ud(ts);
+
+    ctx.translate(x, y);
+    ctx.rotate(Math.atan2(y - hh, x - hw) + π / 2);
     ctx.beginPath();
     ctx.arc(0, 0, 20, π, 0);
     ctx.arc(0, 60, 20, 0, π);
@@ -62,10 +64,10 @@ function draw(ctx, ts) {
     ctx.restore();
 }
 
-(function tick(ts) {
+(function tick(ts = Date.now()) {
     window.requestAnimationFrame(tick);
     ctx.clearRect(0, 0, w, h);
-    for (let i = 0, l = 100; i < l; ++ i) {
+    for (let i = 0, l = 11; i < l; ++ i) {
         draw(ctx, ts + i * (p / l));
     }
 })();
