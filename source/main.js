88d42f54ce2e7550f8b83ed08b7acb24aeafe56b
Matt Hayes <matt@mysterycommand.com>
2016-07-04 15:33:11 -0700

    feat: add part class, rough out some limbs/a walk cycle

--- source/main.js
+++ source/main.js
@@ -7,10 +7,9 @@ const ctx = canvas.getContext('2d');
 const π = Math.PI;
 const ππ = 2 * Math.PI;
 
-let ud, lr;
 let fts, pts, dts;
 let w, h, hw, hh;
-let p = 3000;
+let p = 1500;
 
 function onResize(/*event*/) {
     const {
@@ -22,9 +21,6 @@ function onResize(/*event*/) {
     canvas.width = w = width;
     hh = h / 2;
     hw = w / 2;
-
-    ud = sinWave(0 + hh / 2, h - hh / 2, p);
-    lr = cosWave(0 + hw / 2, w - hw / 2, p);
 }
 
 window.addEventListener('resize', onResize);
@@ -42,18 +38,126 @@ function cosWave(min, max, period, offset = 0) {
     }
 }
 
-function draw(ctx, ts) {
+class Part {
+    constructor({ x, y, r, children, }) {
+        this.x = x || this.x;
+        this.y = y || this.y;
+        this.r = r || this.r;
+
+        this.children = children || [];
+    }
+
+    x(ts) { return 0; }
+    y(ts) { return 0; }
+    r(ts) { return 0; }
+
+    pos(ts) {
+        const x = this.x(ts);
+        const y = this.y(ts);
+        const r = this.r(ts);
+
+        return { x, y, r, };
+    }
+}
+
+const part = new Part({
+    x: function x(ts) { return hw; },
+    y: function y(ts) { return hh - 60; },
+
+    children: [
+        new Part({
+            x: sinWave(-20, 20, p, p / 2),
+
+            y: function y(ts) {
+                return 70;
+            },
+
+            r: function r(ts) {
+                return Math.atan2(this.y(ts) - 60, this.x(ts)) - π / 2;
+            },
+
+            children: [
+                new Part({
+                    y: function y(ts) {
+                        return 60;
+                    },
+
+                    r: sinWave(0, π / 4, p, 500),
+                }),
+            ],
+        }),
+
+        new Part({
+            x: cosWave(-20, 20, p, 0),
+
+            y: function y(ts) {
+                return 70;
+            },
+
+            r: function r(ts) {
+                return Math.atan2(this.y(ts) - 60, this.x(ts)) - π / 2;
+            },
+
+            children: [
+                new Part({
+                    y: function y(ts) {
+                        return 60;
+                    },
+
+                    r: cosWave(0, π / 4, p, 500),
+                }),
+            ],
+        }),
+
+        new Part({
+            x: sinWave(-20, 20, p, p / 2),
+
+            r: function r(ts) {
+                return Math.atan2(this.y(ts) + 10, this.x(ts)) - π / 2;
+            },
+
+            children: [
+                new Part({
+                    y: function y(ts) {
+                        return 60;
+                    },
+
+                    r: sinWave(0, -π / 4, p, 500),
+                }),
+            ],
+        }),
+
+        new Part({
+            x: cosWave(-20, 20, p, 0),
+
+            r: function r(ts) {
+                return Math.atan2(this.y(ts) + 10, this.x(ts)) - π / 2;
+            },
+
+            children: [
+                new Part({
+                    y: function y(ts) {
+                        return 60;
+                    },
+
+                    r: cosWave(0, -π / 4, p, 500),
+                }),
+            ],
+        }),
+    ],
+});
+
+function draw(ctx, ts, part) {
     ctx.save();
 
     ctx.lineWidth = 3;
     ctx.fillStyle = 'rgba(255,255,0,0.65)';
     ctx.strokeStyle = 'rgba(255,0,255,0.85)';
 
-    const x = lr(ts);
-    const y = ud(ts);
-
+    const { x, y, r, } = part.pos(ts);
     ctx.translate(x, y);
-    ctx.rotate(Math.atan2(y - hh, x - hw) - π / 2);
+    ctx.rotate(r);
+
     ctx.beginPath();
     ctx.arc(0, 0, 20, π, 0);
     ctx.arc(0, 60, 20, 0, π);
@@ -61,6 +165,8 @@ function draw(ctx, ts) {
 
     ctx.fill();
     ctx.stroke();
+
+    part.children.forEach((child) => draw(ctx, ts, child));
 
     ctx.restore();
 }
@@ -75,9 +181,7 @@ function draw(ctx, ts) {
     dts = ts - pts;
 
     ctx.clearRect(0, 0, w, h);
-    for (let i = 0, l = 3; i < l; ++ i) {
-        draw(ctx, ts + i * (p / l));
-    }
+    draw(ctx, ts, part);
 
     pts = ts;
 })();
