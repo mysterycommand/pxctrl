ec942f900b56830604c1c1b4ba9633c7f7aed5ee
Matt Hayes <matt@mysterycommand.com>
2016-07-05 13:49:00 -0700

    refactor: make puppets in a factory and render them in a loop, looks like it works okay for lots of puppets

--- source/main.js
+++ source/main.js
@@ -14,51 +14,60 @@ const p0 = 5000;
 const p1 = 8000;
 const p2 = 13000;
 
-const puppet = new Puppet({
-    _x(ts) {
-        return hw - this.width / 2;
-    },
+function makePuppet() {
+    const puppet = new Puppet({
+        _x(ts) {
+            return hw - this.width / 2;
+        },
 
-    _y(ts) {
-        return hh - this.height / 2;
-    },
+        _y(ts) {
+            return hh - this.height / 2;
+        },
 
-    _z: (ts) => 2,
-    _width: (ts) => 80,
-    _height: (ts) => 200,
-});
+        _z: (ts) => 2,
+        _width: (ts) => 80,
+        _height: (ts) => 200,
+    });
 
-puppet.addChild(new Puppet({
-    _x: sine(90, -60, p1),
-    _y: sine(175, -25, p1),
-    _z: sine(3, 1, p1, p1 / 4),
-    _width: (ts) => 50,
-    _height: (ts) => 50,
-}));
+    puppet.addChild(new Puppet({
+        _x: sine(90, -60, p1),
+        _y: sine(175, -25, p1),
+        _z: sine(3, 1, p1, p1 / 4),
+        _width: (ts) => 50,
+        _height: (ts) => 50,
+    }));
 
-puppet.addChild(new Puppet({
-    _x: sine(-60, 90, p1),
-    _y: sine(-25, 175, p1),
-    _z: sine(1, 3, p1, p1 / 4),
-    _width: (ts) => 50,
-    _height: (ts) => 50,
-}));
+    puppet.addChild(new Puppet({
+        _x: sine(-60, 90, p1),
+        _y: sine(-25, 175, p1),
+        _z: sine(1, 3, p1, p1 / 4),
+        _width: (ts) => 50,
+        _height: (ts) => 50,
+    }));
 
-puppet.addChild(new Puppet({
-    _x: cosine(130, -75, p2, p2 / 2),
-    _y: cosine(250, -75, p2),
-    _z: cosine(4, 0, p2, p2 / 4),
-    _width: (ts) => 25,
-    _height: (ts) => 25,
-}));
+    puppet.addChild(new Puppet({
+        _x: cosine(130, -75, p2, p2 / 2),
+        _y: cosine(250, -75, p2),
+        _z: cosine(4, 0, p2, p2 / 4),
+        _width: (ts) => 25,
+        _height: (ts) => 25,
+    }));
 
-puppet.addChild(new Puppet({
-    _x: cosine(-75, 130, p2, p2 / 2),
-    _y: cosine(-75, 250, p2),
-    _z: cosine(0, 4, p2, p2 / 4),
-    _width: (ts) => 25,
-    _height: (ts) => 25,
-}));
+    puppet.addChild(new Puppet({
+        _x: cosine(-75, 130, p2, p2 / 2),
+        _y: cosine(-75, 250, p2),
+        _z: cosine(0, 4, p2, p2 / 4),
+        _width: (ts) => 25,
+        _height: (ts) => 25,
+    }));
+
+    return puppet;
+}
+
+const puppets = [];
+for (let i = 0, l = 1; i < l; ++i) {
+    puppets.push(makePuppet());
+}
 
 function onResize(/*event*/) {
     const {
@@ -71,7 +80,9 @@ function onResize(/*event*/) {
     hh = h / 2;
     hw = w / 2;
 
-    puppet._x = sine(hw / 2, hw + hw / 2, p0);
+    puppets.forEach(puppet => {
+        puppet._x = sine(hw / 2, hw + hw / 2, p0)
+    });
 }
 
 window.addEventListener('resize', onResize);
@@ -87,10 +98,16 @@ onResize();
     dts = ts - pts;
 
     ctx.clearRect(0, 0, w, h);
-    puppet.update(ts);
-    puppet
-        .flat()
-        .forEach(p => p.draw(ctx));
+
+    puppets.forEach((puppet, i) => {
+        const halfLength = (puppets.length - 1) / 2;
+        ctx.globalAlpha = 1 - (Math.abs(i - halfLength) / halfLength * 0.5);
+
+        puppet.update(ts + ((i - halfLength) * (p0 / puppets.length)));
+        puppet
+            .flat()
+            .forEach(p => p.draw(ctx));
+    });
 
     pts = ts;
 })();
