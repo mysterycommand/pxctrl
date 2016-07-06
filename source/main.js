bc92314383b17837aef5d913c20ef76e92fa25da
Matt Hayes <matt@mysterycommand.com>
2016-07-06 08:08:26 -0700

    refactor: remove all the puppet stuff, dink around with colors for a minute

--- source/main.js
+++ source/main.js
@@ -2,7 +2,9 @@ import './lib/style';
 import './main.scss';
 
 import { sine, cosine } from './lib/wave';
-import Puppet from './lib/puppet';
+
+const { PI: π } = Math;
+const ππ = π * 2;
 
 const canvas = document.getElementById('js-canvas');
 const ctx = canvas.getContext('2d');
@@ -10,64 +12,23 @@ const ctx = canvas.getContext('2d');
 let fts, pts, dts;
 let w, h, hw, hh;
 
-const p0 = 5000;
-const p1 = 8000;
-const p2 = 13000;
+const bkgdX = sine(0, 360, 60000);
+const bkgdY = cosine(0, 360, 60000);
+const bkgd = (ts) => {
+    const x = bkgdX(ts);
+    const y = bkgdY(ts);
 
-function makePuppet() {
-    const puppet = new Puppet({
-        _x(ts) {
-            return hw - this.width / 2;
-        },
+    return Math.atan2(y, x) * 180 / π;
+};
 
-        _y(ts) {
-            return hh - this.height / 2;
-        },
+const foreX = sine(0, 360, 60000, 30000);
+const foreY = cosine(0, 360, 60000, 30000);
+const fore = (ts) => {
+    const x = foreX(ts);
+    const y = foreY(ts);
 
-        _z: (ts) => 2,
-        _width: (ts) => 80,
-        _height: (ts) => 200,
-    });
-
-    puppet.addChild(new Puppet({
-        _x: sine(90, -60, p1),
-        _y: sine(175, -25, p1),
-        _z: sine(3, 1, p1, p1 / 4),
-        _width: (ts) => 50,
-        _height: (ts) => 50,
-    }));
-
-    puppet.addChild(new Puppet({
-        _x: sine(-60, 90, p1),
-        _y: sine(-25, 175, p1),
-        _z: sine(1, 3, p1, p1 / 4),
-        _width: (ts) => 50,
-        _height: (ts) => 50,
-    }));
-
-    puppet.addChild(new Puppet({
-        _x: cosine(130, -75, p2, p2 / 2),
-        _y: cosine(250, -75, p2),
-        _z: cosine(4, 0, p2, p2 / 4),
-        _width: (ts) => 25,
-        _height: (ts) => 25,
-    }));
-
-    puppet.addChild(new Puppet({
-        _x: cosine(-75, 130, p2, p2 / 2),
-        _y: cosine(-75, 250, p2),
-        _z: cosine(0, 4, p2, p2 / 4),
-        _width: (ts) => 25,
-        _height: (ts) => 25,
-    }));
-
-    return puppet;
-}
-
-const puppets = [];
-for (let i = 0, l = 1; i < l; ++i) {
-    puppets.push(makePuppet());
-}
+    return Math.atan2(y, x) * 180 / π;
+};
 
 function onResize(/*event*/) {
     const {
@@ -79,16 +40,12 @@ function onResize(/*event*/) {
     canvas.width = w = width;
     hh = h / 2;
     hw = w / 2;
-
-    puppets.forEach(puppet => {
-        puppet._x = sine(hw / 2, hw + hw / 2, p0)
-    });
 }
 
 window.addEventListener('resize', onResize);
 onResize();
 
-(function tick(ts = Date.now()) {
+(function tick(ts = performance.now()) {
     window.requestAnimationFrame(tick);
 
     fts || (fts = ts);
@@ -96,18 +53,15 @@ onResize();
 
     ts -= fts;
     dts = ts - pts;
+    // ts = Math.random() * 60000;
+    // console.log(ts.toFixed(2), bkgd(ts).toFixed(2), fore(ts).toFixed(2));
+    console.log(ts);
 
-    ctx.clearRect(0, 0, w, h);
+    ctx.fillStyle = `hsl(${bkgd(ts)}, 50%, 50%)`;
+    ctx.fillRect(0, 0, w, h);
 
-    puppets.forEach((puppet, i) => {
-        const halfLength = (puppets.length - 1) / 2;
-        ctx.globalAlpha = 1 - (Math.abs(i - halfLength) / halfLength * 0.5);
-
-        puppet.update(ts + ((i - halfLength) * (p0 / puppets.length)));
-        puppet
-            .flat()
-            .forEach(p => p.draw(ctx));
-    });
+    ctx.fillStyle = `hsl(${fore(ts)}, 50%, 50%)`;
+    ctx.fillRect(hw - 50, hh - 50, 100, 100);
 
     pts = ts;
 })();
