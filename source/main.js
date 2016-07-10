95ae2712060e713555035c53f0c4c04fbb1f35f0
Matt Hayes <matt@mysterycommand.com>
2016-07-10 13:43:44 -0700

    refactor: remove the color stuff for now, rework the timing stuff so there's always a value and the loop always starts at 0

--- source/main.js
+++ source/main.js
@@ -3,32 +3,11 @@ import './main.scss';
 
 import { sine, cosine } from './lib/wave';
 
-const { PI: π } = Math;
-const ππ = π * 2;
-
 const canvas = document.getElementById('js-canvas');
 const ctx = canvas.getContext('2d');
 
-let fts, pts, dts;
+let fts = -1, pts = -1, dts = -1;
 let w, h, hw, hh;
-
-const bkgdX = sine(0, 360, 60000);
-const bkgdY = cosine(0, 360, 60000);
-const bkgd = (ts) => {
-    const x = bkgdX(ts);
-    const y = bkgdY(ts);
-
-    return Math.atan2(y, x) * 180 / π;
-};
-
-const foreX = sine(0, 360, 60000, 30000);
-const foreY = cosine(0, 360, 60000, 30000);
-const fore = (ts) => {
-    const x = foreX(ts);
-    const y = foreY(ts);
-
-    return Math.atan2(y, x) * 180 / π;
-};
 
 function onResize(/*event*/) {
     const {
@@ -45,23 +24,25 @@ function onResize(/*event*/) {
 window.addEventListener('resize', onResize);
 onResize();
 
-(function tick(ts = performance.now()) {
-    window.requestAnimationFrame(tick);
+function tick(ts) {
+    // window.requestAnimationFrame(tick);
 
-    fts || (fts = ts);
-    pts || (pts = fts);
-
+    if (fts === -1) { fts = ts; }
     ts -= fts;
+
+    if (pts === -1) { pts = ts; }
+    if (dts === -1) { dts = ts; }
     dts = ts - pts;
-    // ts = Math.random() * 60000;
-    // console.log(ts.toFixed(2), bkgd(ts).toFixed(2), fore(ts).toFixed(2));
-    console.log(ts);
 
-    ctx.fillStyle = `hsl(${bkgd(ts)}, 50%, 50%)`;
-    ctx.fillRect(0, 0, w, h);
-
-    ctx.fillStyle = `hsl(${fore(ts)}, 50%, 50%)`;
-    ctx.fillRect(hw - 50, hh - 50, 100, 100);
+    // do work with ts or dts here
+    // console.log('fts:', fts.toFixed(3));
+    // console.log('pts:', pts.toFixed(3));
+    // console.log(' ts:', ts.toFixed(3));
+    // console.log('dts:', dts.toFixed(3));
+    // console.log('fps:', (1000 / dts).toFixed(3));
+    // console.log('');
 
     pts = ts;
-})();
+};
+
+window.requestAnimationFrame(tick);
