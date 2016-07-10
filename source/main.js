6e2bb7f7f8102dfd007e2b502f81d732ace1fd02
Matt Hayes <matt@mysterycommand.com>
2016-07-10 14:35:28 -0700

    refactor: fill the canvas with a color who's hue loops through 360 degrees over 12 seconds, draw a smaller square with the background color's compliment

--- source/main.js
+++ source/main.js
@@ -12,6 +12,10 @@ const ππ = π * 2;
 let fts = -1, pts = -1, dts;
 let w, h, hw, hh;
 
+const p = 12000;
+const fx = cosine(-1, 1, p);
+const fy = sine(-1, 1, p);
+
 function onResize(/*event*/) {
     const {
         innerHeight: height,
@@ -39,6 +43,20 @@ function tick(ts) {
 
     if (pts === -1) { pts = ts; }
     dts = ts - pts;
+
+    const x = fx(ts);
+    const y = fy(ts);
+    const a = Math.atan2(y, x);
+    const d = toDeg(a) + 90;
+    const e = (d + 180) % 360;
+
+    ctx.fillStyle = `hsl(${d},70%,30%)`;
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.fillStyle = `hsl(${e},70%,30%)`;
+    ctx.shadowBlur = 200;
+    ctx.shadowColor = 'rgba(0,0,0,0.65)';
+    ctx.fillRect(hw - 100, hh - 100, 200, 200);
 
     pts = ts;
 };
