f37025db7582ba0390aa751da6cb64c2048d9434
Matt Hayes <matt@mysterycommand.com>
2016-07-10 14:13:44 -0700

    refactor: draws a color wheel with radius 'r' and 'p' stops

--- source/main.js
+++ source/main.js
@@ -6,8 +6,13 @@ import { sine, cosine } from './lib/wave';
 const canvas = document.getElementById('js-canvas');
 const ctx = canvas.getContext('2d');
 
+const { PI: π } = Math;
+const ππ = π * 2;
+
 let fts = -1, pts = -1, dts;
 let w, h, hw, hh;
+
+let fx, fy, r = 100, p = 128;
 
 function onResize(/*event*/) {
     const {
@@ -19,10 +24,17 @@ function onResize(/*event*/) {
     canvas.width = w = width;
     hh = h / 2;
     hw = w / 2;
+
+    fx = cosine(hw - r, hw + r, p);
+    fy = sine(hh - r, hh + r, p);
 }
 
 window.addEventListener('resize', onResize);
 onResize();
+
+function toDeg(rad) {
+    return rad * 180 / π;
+}
 
 function tick(ts) {
     // window.requestAnimationFrame(tick);
@@ -34,12 +46,23 @@ function tick(ts) {
     dts = ts - pts;
 
     // do work with ts or dts here
-    // console.log('fts:', fts.toFixed(3));
-    // console.log('pts:', pts.toFixed(3));
-    // console.log(' ts:', ts.toFixed(3));
-    // console.log('dts:', dts.toFixed(3));
-    // console.log('fps:', (1000 / dts).toFixed(3));
-    // console.log('');
+    ctx.clearRect(0, 0, w, h);
+    for (let i = 0; i < p; ++i) {
+        ctx.save();
+
+        let x = fx(i);
+        let y = fy(i);
+        let a = Math.atan2(y - hh, x - hw);
+        let h = (ππ * r) / p;
+
+        ctx.translate(x, y);
+        ctx.rotate(a);
+
+        ctx.fillStyle = `hsl(${toDeg(a) + 90},100%,50%)`;
+        ctx.fillRect(0, -h / 2, 25, h);
+
+        ctx.restore();
+    }
 
     pts = ts;
 };
