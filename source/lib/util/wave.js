0f65d8a9f26c21d9517f930194e82aecda7ce4af
Matt Hayes <matt@mysterycommand.com>
2016-08-06 15:38:09 -0700

    style: shorten the argument name, update docs

--- source/lib/util/wave.js
+++ source/lib/util/wave.js
@@ -3,8 +3,8 @@ import { ππ, sin, cos } from './math';
 /**
  * ## getWaveFn
  * this utility function returns a wave function that takes a timestamp
- * (optionally offset by `offset`) and returns a value between `min` and `max`
- * for the period `p`
+ * (optionally offset by `o`) and returns a value between `min` and `max` for
+ * the period `p`
  *
  * @param  {Function} fn     the function to generate the wave (takes a value in
  *                           radians and returns a value between -1 and 1 at
@@ -12,14 +12,14 @@ import { ππ, sin, cos } from './math';
  * @param  {Number}   p      the period for this wave (in milliseconds)
  * @param  {Number}   min    the minimum value for the generated wave
  * @param  {Number}   max    the maximum value for the generated wave
- * @param  {Number}   offset an optional offset (in milliseconds) at which to
+ * @param  {Number}   o      an optional offset (in milliseconds) at which to
  *                           start the wave
  *
  * @return {Function}        a wave function that takes a timestamp (optionally
- *                           offset by `offset`) and returns a value between
- *                           `min` and `max` for the period `p`
+ *                           offset by `o`) and returns a value between `min`
+ *                           and `max` for the period `p`
  */
-export function getWaveFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
+export function getWaveFn(fn, p = 1000, min = -1, max = 1, o = 0) {
     // peak amplitude (not peak-to-peak amplitude)
     // @see https://en.wikipedia.org/wiki/Amplitude
     const amp = (max - min) / 2;
@@ -30,7 +30,7 @@ export function getWaveFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
 
     return (ts) => {
         // offset timestamp
-        const ots = offset + ts;
+        const ots = o + ts;
         return amp * (1 + fn(ots * rpp)) + min;
     };
 }
@@ -38,8 +38,8 @@ export function getWaveFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
 /**
  * ## getStepFn
  * this utility function returns a step function (like a integer-only wave
- * function) that takes a timestamp (optionally offset by `offset`) and returns
- * an integer value between `min` and `max` for the period `p`
+ * function) that takes a timestamp (optionally offset by `o`) and returns an
+ * integer value between `min` and `max` for the period `p`
  *
  * @param  {Function} fn     the function to generate the stepped wave (takes a
  *                           value in radians and returns a value between -1 and
@@ -47,17 +47,17 @@ export function getWaveFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
  * @param  {Number}   p      the period for this wave (in milliseconds)
  * @param  {Number}   min    the minimum value for the generated wave
  * @param  {Number}   max    the maximum value for the generated wave
- * @param  {Number}   offset an optional offset (in milliseconds) at which to
+ * @param  {Number}   o      an optional offset (in milliseconds) at which to
  *                           start the wave
  *
  * @return {Function}        a wave function that takes a timestamp (optionally
- *                           offset by `offset`) and returns an integer value
- *                           between `min` and `max` for the period `p`
+ *                           offset by `o`) and returns an integer value between
+ *                           `min` and `max` for the period `p`
  */
-export function getStepFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
+export function getStepFn(fn, p = 1000, min = -1, max = 1, o = 0) {
     // the root wave function that generates a value between min and max over
     // a period p and optionally offset by o
-    const waveFn = getWaveFn(fn, p, min, max, offset);
+    const waveFn = getWaveFn(fn, p, min, max, o);
 
     // just floors the result of the above function
     return (ts) => Math.floor(waveFn(ts));
@@ -66,37 +66,37 @@ export function getStepFn(fn, p = 1000, min = -1, max = 1, offset = 0) {
 /**
  * ## getSinFn
  * returns a sine wave function that takes a timestamp (optionally offset by
- * `offset`) and returns a value between `min` and `max` for the period `p`
+ * `o`) and returns a value between `min` and `max` for the period `p`
  *
  * @param  {Number}   p      the period for this sine wave (in milliseconds)
  * @param  {Number}   min    the minimum value for the generated sine wave
  * @param  {Number}   max    the maximum value for the generated sine wave
- * @param  {Number}   offset an optional offset (in milliseconds) at which to
+ * @param  {Number}   o      an optional offset (in milliseconds) at which to
  *                           start the sine wave
  *
  * @return {Function}        a sine wave function that takes a timestamp
- *                           (optionally offset by `offset`) and returns a value
+ *                           (optionally offset by `o`) and returns a value
  *                           between `min` and `max` for the period `p`
  */
-export function getSinFn(p = 1000, min = -1, max = 1, offset = 0) {
-    return getWaveFn(sin, p, min, max, offset);
+export function getSinFn(p = 1000, min = -1, max = 1, o = 0) {
+    return getWaveFn(sin, p, min, max, o);
 }
 
 /**
  * ## getCosFn
  * returns a cosine wave function that takes a timestamp (optionally offset by
- * `offset`) and returns a value between `min` and `max` for the period `p`
+ * `o`) and returns a value between `min` and `max` for the period `p`
  *
  * @param  {Number}   p      the period for this cosine wave (in milliseconds)
  * @param  {Number}   min    the minimum value for the generated cosine wave
  * @param  {Number}   max    the maximum value for the generated cosine wave
- * @param  {Number}   offset an optional offset (in milliseconds) at which to
+ * @param  {Number}   o      an optional offset (in milliseconds) at which to
  *                           start the cosine wave
  *
  * @return {Function}        a cosine wave function that takes a timestamp
- *                           (optionally offset by `offset`) and returns a value
+ *                           (optionally offset by `o`) and returns a value
  *                           between `min` and `max` for the period `p`
  */
-export function getCosFn(p = 1000, min = -1, max = 1, offset = 0) {
-    return getWaveFn(cos, p, min, max, offset);
+export function getCosFn(p = 1000, min = -1, max = 1, o = 0) {
+    return getWaveFn(cos, p, min, max, o);
 }
