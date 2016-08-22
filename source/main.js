55bcdc5492592ea0670e640da97f11075e532996
Matt Hayes <matt@mysterycommand.com>
2016-08-22 11:26:39 -0700

    refactor: move request animation frame setup (tick) into it's own module

--- source/main.js
+++ source/main.js
@@ -1,56 +1,11 @@
 import './lib/style';
 import './main.scss';
 
+import resize from './lib/on/resize';
+import tick from './lib/on/tick';
+
 import { tri } from './lib/util/math';
 import { getStepFn } from './lib/util/wave';
-
-const cvs = document.getElementById('js-canvas');
-const ctx = cvs.getContext('2d');
-
-const frameFn = getStepFn(tri, 500, 0.5, 2.5);
-
-let fts = -1, pts = -1, dts;
-let w, h, hw, hh, frames;
-
-import resize from './lib/on/resize';
-resize(window, function ({ width, height, centerX, centerY }) {
-    cvs.width = w = width;
-    cvs.height = h = height;
-    hw = centerX;
-    hh = centerY;
-});
-
-function tick(ts) {
-    requestAnimationFrame(tick);
-
-    // if there is no 'first timestamp' use the current one
-    if (fts === -1) { fts = ts; }
-
-    // make this 'timestamp' relative to the 'first timestamp'
-    ts -= fts;
-
-    // if there is no 'previous timestamp' use the current one
-    if (pts === -1) { pts = ts; }
-
-    // make the 'delta timestamp' the difference between this 'timestamp'
-    // and the 'previous timestamp' (will be 0 on the first frame)
-    dts = ts - pts;
-
-    // do work with ts or dts here
-    ctx.clearRect(0, 0, w, h);
-    ctx.imageSmoothingEnabled = false;
-
-    const f = frameFn(ts);
-    ctx.drawImage(frames[f], hw - 96, hh - 96, 192, 192);
-
-    ctx.fillStyle = '#fff';
-    ctx.font = '24px monospace';
-    ctx.fillText((1000 / dts).toFixed(2), 8, 28);
-    ctx.fillText(f, 8, 56);
-
-    // update the 'previous timestamp'
-    pts = ts;
-}
 
 import load from './lib/on/load';
 load([
@@ -58,6 +13,31 @@ load([
     './images/megaman-01.png',
     './images/megaman-02.png',
 ], results => {
-    frames = results;
-    requestAnimationFrame(tick);
+    const cvs = document.getElementById('js-canvas');
+    const ctx = cvs.getContext('2d');
+
+    const frameFn = getStepFn(tri, 500, 0.5, 2.5);
+
+    let fts = -1, pts = -1, dts;
+    let w, h, hw, hh, frames = results;
+
+    resize(window, function ({ width, height, centerX, centerY }) {
+        cvs.width = w = width;
+        cvs.height = h = height;
+        hw = centerX;
+        hh = centerY;
+    });
+
+    tick(function ({ ts, dts }) {
+        ctx.clearRect(0, 0, w, h);
+        ctx.imageSmoothingEnabled = false;
+
+        const f = frameFn(ts);
+        ctx.drawImage(frames[f], hw - 96, hh - 96, 192, 192);
+
+        ctx.fillStyle = '#fff';
+        ctx.font = '24px monospace';
+        ctx.fillText((1000 / dts).toFixed(2), 8, 28);
+        ctx.fillText(f, 8, 56);
+    });
 });
